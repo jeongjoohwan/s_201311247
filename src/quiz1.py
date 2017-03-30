cadef86c2dcaae28da6dba12b392458521235c7d
jeongjoohwan <jbsp1701@gmail.com>
2017-03-30 21:52:13 +0900

    Add files via upload

---
+++ src/quiz1.py
@@ -0,0 +1,13 @@
+#coding: utf-8
+import requests
+rResponse = requests.get('http://python.org/')
+_html = rResponse.text
+print len(_html)
+import re
+
+re.compile('http://.+"')
+p=re.compile('href="(http://.*?)"')
+nodes=p.findall(_html)
+print "http url은 몇 개?",len(nodes)
+for i, node in enumerate(nodes):
+    print i, node
